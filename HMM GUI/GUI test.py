33427835fdc09c332ed64218f3404865a3be1573
bojongle <bojongle@proton.me>
2022-09-20 15:44:17 -0500

    Update GUI test.py

--- HMM GUI/GUI test.py
+++ HMM GUI/GUI test.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from distutils.cmd import Command
 from mailbox import Mailbox
 from msilib import Win64
 from struct import pack
@@ -19,7 +20,7 @@ def utiligram_func2():
     win11.title("Utiligram")
     win11.resizable(True, True)
     win11_l1 = Label(win11, text="Utiligram is a lightweight Metaploit GUI", bg="Blue", fg="Green", font=("Roboto Mono", 30))
-    
+    win11
     
     win11.mainloop()
 
@@ -129,10 +130,10 @@ def Utiligram_Func():
     win3.resizable(True, True)
     win3.title("Utiligram Installer")
     win3_l1 = Label(win3, text="Select the version below")
-    win3_butt1 = Button(win3, text="FULL INSTALL" command=Utiligram_Full_Func)
-    win3_butt2 = Button(win3, text="Lightweight Install", command=Utiligram_Lightweight_func)
+    win3_butt1 = Button(win3, text="FULL INSTALL")
+    win3_butt2 = Button(win3, text="Lightweight Install")
     win3_butt3 = Button (win3, text="Minimal Install")
-    win3_butt4 = Button (win3, text="back" command=MAIN_PAGE)
+    win3_butt4 = Button (win3, text="back")
     
 
 
@@ -199,7 +200,7 @@ def Unigram_Func():
     win8.title("Unigram")
     win8.resizable(True, True)
     win8_l1 = Label(win8, text="Unigram is a Instagram botnet GUI", bg="Black", fg="Green", font=("Roboto Mono", 30))
-    win8_butt1 = Button(win8, text="Download .exe", command=Unigram_Func2)
+    win8_butt1 = Button(win8, text="Download .exe", command=utiligram_func2)
     win8_l1.pack()
     win8.mainloop()
 
@@ -224,11 +225,13 @@ Triple_G = Button(MAIN_PAGE, text="Triple G", command=Triple_G_Func, bg="Black",
 LogRolling = Button(MAIN_PAGE, text="Log Rolling", command=LogRoller_Func, bg="Light Grey", fg="Green", font=("Arial", 20)).grid(column=1, row=0)
 Near_Field_Communication = Button(MAIN_PAGE, text="Near Field Communication", command=NFC_func, bg="Blue", fg="Red", font=("Arial", 20)).grid(column=1, row=1)
 Unigram = Button(MAIN_PAGE, text="Unigram", command=Unigram_Func, bg="Red", fg="Orange", font=("Arial", 20)).grid(column=1, row=2)
-
+TensorMole = Button(MAIN_PAGE, text=""))
 
 
 MAIN_PAGE.mainloop()
 
 
 
-def 
+def backButt():
+    global backButt
+backbutt = command=MAIN_PAGE
